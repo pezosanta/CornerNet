32c7ece8cee67c31fe098b14c2bf4784060624c3
pezosanta <pezosanta@gmail.com>
2020-03-12 17:11:29 +0000

    added a new func for creating tensorboard graph

--- test_v2.py
+++ test_v2.py
@@ -8,6 +8,7 @@ from torch.utils.tensorboard import SummaryWriter
 from pprint import pprint
 from collections import defaultdict
 from NonMaxSuppression.nms import soft_nms, soft_nms_merge
+from CornerNet import kp as cornernet
 
 
 train_annotation_path       = "../BDD100K/bdd100k_labels_images_train.json"
@@ -18,17 +19,6 @@ map_detection_path          = "../Detections/Hourglass/"
 gt_detection_path           = "../BDD100K/detection_val.json"
 
 train_val_image_root = "../BDD100K/train/"
-
-categories_dict = { 0: 'bus',
-                    1: 'traffic light',
-                    2: 'traffic sign',
-                    3: 'person',
-                    4: 'bicycle',
-                    5: 'truck',
-                    6: 'motorcycle',
-                    7: 'car',
-                    8: 'train',
-                    9: 'rider' }
 
 reverse_categories_dict = { 0: 'bus',
                             1: 'traffic light',
@@ -66,16 +56,16 @@ def separate_train_annotations():
         json.dump(annotation_file[0:60000], f, ensure_ascii = False, indent = 4)        # Then rename the new json file to "bdd100k_labels_images_train.json
 
 def save_detections(detections_json, current_epoch):
-    with open(map_detection_path + "hourglass_map_detections_epoch_{}.json".format(current_epoch), 'w') as f:
+    with open(map_detection_path + "cornernet_hourglass_map_detections_epoch_{}.json".format(current_epoch), 'w') as f:
         json.dump(detections_json, f, ensure_ascii = False, indent = 4, cls = MyJSONEncoder)
 
-def generate_detections(detections, names, orig_images, detections_json, nms_threshold, min_score, current_iter, mean_ap_iterations, \
+def generate_detections(detections, names, orig_images, detections_json, nms_threshold, min_score, current_iter, annot_image_saving_iterations, \
                             pred_detections_images, gt_detections_images):
     out_width           = 160
     categories          = 10
     max_per_image       = 100
     merge_bbox          = False
-    nms_algorithm       = 2                                                                     # "exp_soft_nms"
+    nms_algorithm       = 2                                # "exp_soft_nms"
     weight_exp          = 8
 
     width_ratio         = orig_size[1] / output_size[1]                                         
@@ -94,11 +84,10 @@ def generate_detections(detections, names, orig_images, detections_json, nms_thr
     classes             = classes[keep_inds]
 
     top_bboxes = {}
-    for j in range(0, categories):                                                          #??????????????
-            keep_inds = (classes == j)
-            #print('KEEP_INDS: {}'.format(keep_inds))
-            
+    for j in range(0, categories):                                                          
+            keep_inds = (classes == j)            
             top_bboxes[j] = detections[keep_inds][:, 0:7].astype(np.float32)
+
             if merge_bbox:
                 soft_nms_merge(top_bboxes[j], Nt=nms_threshold, method=nms_algorithm, weight_exp=weight_exp)
             else:
@@ -116,7 +105,7 @@ def generate_detections(detections, names, orig_images, detections_json, nms_thr
             keep_inds = (top_bboxes[j][:, -1] >= thresh)
             top_bboxes[j] = top_bboxes[j][keep_inds]
     
-    # Keep only score > min_score bboxes
+    # Keep only (score > min_score) bboxes
     for j in range(0, categories):
         keep_inds = (top_bboxes[j][:, -1] > min_score)    
         top_bboxes[j] = top_bboxes[j][keep_inds]
@@ -135,21 +124,23 @@ def generate_detections(detections, names, orig_images, detections_json, nms_thr
     print('RIDER_BBOXES_LEN: {}'.format(len(top_bboxes[9])))
     '''    
 
+    # Save the detected bounding boxes into detections_json
     for j in range(0, categories):
         for bbox in top_bboxes[j]:
-            bbox[0:4:2] = bbox[0:4:2] * width_ratio
+            bbox[0:4:2] = bbox[0:4:2] * width_ratio             # Transform bbox coordinates to the original image size (96x160 --> 720x1280)
             bbox[1:4:2] = bbox[1:4:2] * height_ratio
             detections_json.append(
                 {
                     "name": names[0],
                     "timestamp": 10000,
-                    "category": categories_dict[j],
+                    "category": reverse_categories_dict[j],
                     "bbox": bbox[0:4],
                     "score": 1
                 }
             )   
 
-    if current_iter in (mean_ap_iterations[0], mean_ap_iterations[1], mean_ap_iterations[2], mean_ap_iterations[3]):
+    # Create and save annotated image from predictions along with the corresponding gt annotated image
+    if current_iter in (annot_image_saving_iterations[0], annot_image_saving_iterations[1], annot_image_saving_iterations[2], annot_image_saving_iterations[3]):
         pred_image = generate_annotated_image(detections = top_bboxes, mode = 'PRED', image = None, image_name = names[0])
         pred_detections_images.append(torch.from_numpy(pred_image / 255.0).float().unsqueeze(dim = 0).permute(0,3,1,2))
 
@@ -157,7 +148,7 @@ def generate_detections(detections, names, orig_images, detections_json, nms_thr
 
 def generate_annotated_image(detections, mode, image = None, image_name = None):
     num_categories = 10
-
+    
     if mode == 'GT':
         top_bboxes = []
         for i in range(num_categories):
@@ -186,7 +177,7 @@ def generate_annotated_image(detections, mode, image = None, image_name = None):
         color     = color.astype(np.int32).tolist()
         for bbox in top_bboxes[i]:
             bbox  = (bbox[0:4]).astype(np.int32)
-            bbox[0:4:2] = np.clip(bbox[0:4:2], 1, (orig_size[1] - 1))
+            bbox[0:4:2] = np.clip(bbox[0:4:2], 1, (orig_size[1] - 1))                   # This way the bboxes looks nicer on the edges
             bbox[1:4:2] = np.clip(bbox[1:4:2], 1, (orig_size[0] - 1))
             if bbox[1] - cat_size[1] - 2 < 0:
                 cv2.rectangle(annot_image,
@@ -216,9 +207,6 @@ def generate_annotated_image(detections, mode, image = None, image_name = None):
     return annot_image
 
 def make_grid_image(gt_detections_images, pred_detections_images):
-    print(pred_detections_images[0].shape)
-    print(gt_detections_images[0].shape)
-
     concat_gt_image     = torch.cat((gt_detections_images[0], gt_detections_images[1], gt_detections_images[2], gt_detections_images[3]), 0)
     concat_pred_image   = torch.cat((pred_detections_images[0], pred_detections_images[1], pred_detections_images[2], pred_detections_images[3]), 0)
     
@@ -327,7 +315,7 @@ def cat_pc(gt, predictions, thresholds):
 
 
 def evaluate_detections(current_epoch, mean_ap_thresholds):
-    result_path = map_detection_path + "hourglass_map_detections_epoch_{}.json".format(current_epoch)
+    result_path = map_detection_path + "cornernet_hourglass_map_detections_epoch_{}.json".format(current_epoch)
     thresholds = mean_ap_thresholds
 
     gt = json.load(open(gt_detection_path, 'r'))
@@ -349,3 +337,43 @@ def evaluate_detections(current_epoch, mean_ap_thresholds):
     #      ', '.join(['{:.2f}'.format(n) for n in breakdown]))
     
     return mean_ap, breakdown, cat_list
+
+def create_graph():
+    '''
+    To use this function, first some modifications are required in (CornerNet.py / kp).
+    1.) Modify the forward function's inputs as below:
+            def forward(self, image, tl_tags, br_tags, mode = 'Train, ae_threshold = 0.5, top_k = 100, kernel = 3):
+            if ((mode == 'Train') or (mode == 'Val')):
+                image   = image
+                tl_inds = tl_tags
+                br_inds = br_tags
+    2.) Modify the forward function's return statement as below:
+            return outs[0], outs[1], outs[2], outs[3], outs[4], outs[5]
+    '''
+
+    writer = SummaryWriter('logs/cornernet_hourglass_graph/') 
+
+    # Model Hyperparameters
+    n = 5
+    nstack = 2
+    dims    = [256, 256, 384, 384, 384, 512]
+    modules = [2, 2, 2, 2, 2, 4]
+    out_dim = 10
+
+    model = cornernet(n = n, nstack = nstack, dims = dims, modules = modules, out_dim = out_dim).cuda()
+
+    # Creating default inputs 
+    image       = torch.zeros(1, 3, 384, 640).float().cuda()
+    tl_tags     = torch.from_numpy(np.zeros((1, 160), dtype = np.int64)).cuda()
+    br_tags     = torch.from_numpy(np.zeros((1, 160), dtype = np.int64)).cuda()
+
+    writer.add_graph(model, input_to_model=(image, tl_tags, br_tags), verbose = True)
+    writer.close()
+    
+    # Some errors occur here because of inplace operations in Corner_pooling.py
+    torch.onnx.export(model = model, args = (image, tl_tags, br_tags), f = "cornernet.onnx", verbose = False, opset_version = 11, export_params = True, 
+        input_names = ['image', 'tl_tags', 'br_tags'], output_names = ['tl_heat', 'br_heat', 'tl_tag', 'br_tag', 'tl_regr', 'br_regr'])
+
+
+if __name__ == "__main__":
+    create_graph()
